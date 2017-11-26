ba0423d2cbf9ff83ab4a0d106fb6a0ebe724992f
Andrew Weilbacher <andrew.weilbacher@gmail.com>
2017-11-25 21:48:10 -0500

    started routes

--- WhereToLive/server.js
+++ WhereToLive/server.js
@@ -1,17 +1,28 @@
 // server.js
-// load the things we need
 var express = require('express');
+var ejs = require('ejs');
+var path = require('path');
 var app = express();
 
-// set the view engine to ejs
+// Set default directories
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Use assets in public folder
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 // use res.render to load up an ejs view file
 
 // index page 
-app.get('/', function(req, res) {
-    res.render('views/index');
+app.get('/', function (req, res) {
+    res.render('index');
 });
 
-app.listen(8080);
-console.log('8080 is the magic port');
+// About page
+app.get('/about', function (req, res) {
+    res.render('index');
+});
+
+app.listen(3000, function() {
+  console.log('listening on 3000')
+})
